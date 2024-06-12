71aab6c1cd796d14a4602c1b855334f7d1d6b1c1
RaghavOG <04raghavsingla28@gmail.com>
2024-06-12 10:05:17 +0530

    2769. Find the Maximum Achievable Number

---
+++ easy/2769-Find-the-Maximum-Achievable-Number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 2769. Find the Maximum Achievable Number
+// https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+};
